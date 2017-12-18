bc51ea85681b0d3ac55ecbdf8774487a7c3f4f7c
Brett Vickers <brett@phillips-vickers.com>
2017-12-18 10:58:39 -0500

    Implement friendly error messages from parsing invalid header rows.

--- lib/csv_decision/parse_header.rb
+++ lib/csv_decision/parse_header.rb
@@ -25,7 +25,7 @@ module CSVDecision
 
     def self.column?(cell:)
       match = Header::COLUMN_TYPE.match(cell)
-      raise CellValidationError, "header row column name '#{cell}' is not valid" unless match
+      raise CellValidationError, 'column name is not well formed' unless match
 
       column_type = column_type(type: match['type'])
       column_name = column_name(type: column_type, name: match['name'])
@@ -34,20 +34,18 @@ module CSVDecision
 
     rescue CellValidationError => exp
       raise CellValidationError,
-            "header row column name '#{cell}' is not valid due to #{exp.message}"
+            "header column '#{cell}' is not valid as #{exp.message}"
     end
 
     def self.column_type(type:)
-      return type.downcase.to_sym if type.present?
-
-      raise CellValidationError, 'invalid column type'
+      return type.downcase.to_sym
     end
 
     def self.column_name(type:, name:)
       return format_column_name(name) if name.present?
       return if COLUMN_TYPE_ANONYMOUS.member?(type)
 
-      raise CellValidationError, 'invalid or missing column name'
+      raise CellValidationError, 'the column name is missing'
     end
 
     def self.format_column_name(name)
@@ -55,7 +53,7 @@ module CSVDecision
 
       return column_name.to_sym if COLUMN_NAME.match(column_name)
 
-      raise CellValidationError, "invalid column name #{name}"
+      raise CellValidationError, "column name '#{name}' contains invalid characters"
     end
   end
 end
--- spec/csv_decision/header_spec.rb
+++ spec/csv_decision/header_spec.rb
@@ -26,5 +26,49 @@ describe CSVDecision::Header do
     # expect(result.header.ins).to eq expected
   end
 
+  it 'rejects an invalid column header' do
+    data = [
+      ['IN :input', 'BAD :output'],
+      ['input', '']
+    ]
 
+    expect { CSVDecision.parse(data) }
+      .to raise_error(CSVDecision::CellValidationError,
+                      "header column 'BAD :output' is not valid as " \
+                      'column name is not well formed')
+  end
+
+  it 'rejects a missing column name' do
+    data = [
+      ['IN :input', 'IN: '],
+      ['input', '']
+    ]
+
+    expect { CSVDecision.parse(data) }
+      .to raise_error(CSVDecision::CellValidationError,
+                      "header column 'IN:' is not valid as the column name is missing")
+  end
+
+  it 'rejects an invalid column name' do
+    data = [
+      ['IN :input', 'IN: a-b'],
+      ['input', '']
+    ]
+
+    expect { CSVDecision.parse(data) }
+      .to raise_error(CSVDecision::CellValidationError,
+                      "header column 'IN: a-b' is not valid as " \
+                      "column name 'a-b' contains invalid characters")
+  end
+
+  context 'rejects invalid CSV file decision table headers' do
+    Dir[File.join(CSVDecision.root, 'spec/data/invalid/invalid_header*.csv')].each do |file_name|
+      pathname = Pathname(file_name)
+
+      it "rejects CSV file #{pathname.basename}" do
+        expect { CSVDecision.parse(pathname) }
+          .to raise_error(CSVDecision::FileError, /\Aerror processing CSV file/)
+      end
+    end
+  end
 end
