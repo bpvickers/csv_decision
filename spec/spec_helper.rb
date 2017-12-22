ed6e1b4c99ebd551a6d58c5f34952b44f68edadf
Brett Vickers <brett@phillips-vickers.com>
2017-12-22 16:35:30 -0500

    Get Coveralls working.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'active_support'
+require 'active_support/core_ext'
 require 'json'
 require 'coveralls'
 Coveralls.wear!
