cace74162cca190a3c1e670def51dba13ad9c403
Brett Vickers <brett@phillips-vickers.com>
2017-12-23 15:34:20 -0500

    Refactor code.

--- lib/csv_decision/matchers/function.rb
+++ lib/csv_decision/matchers/function.rb
@@ -19,11 +19,16 @@ module CSVDecision
       return false unless match['args'] == ''
       return false unless match['negate'] == ''
 
+      constant?(match)
+    end
+
+    def self.constant?(match)
       name = match['name'].to_sym
       return false unless NON_NUMERIC_CONSTANTS.key?(name)
 
       Proc.with(type: :constant, function: NON_NUMERIC_CONSTANTS[name])
     end
+    private_class_method :constant?
 
     # Match cell against a function call or symbolic expression.
     class Function < Matcher
@@ -32,7 +37,7 @@ module CSVDecision
       # := function(arg: symbol)
       # == :column_name
       FUNCTION_CALL =
-        "(?<operator>=|:=|==|<|>|!=|>=|<=|:|!\\s*:)\s*(?<negate>!?)\\s*(?<name>#{Header::COLUMN_NAME}|:)(?<args>.*)"
+        "(?<operator>=|:=|==|=|<|>|!=|>=|<=|:|!\\s*:)\s*(?<negate>!?)\\s*(?<name>#{Header::COLUMN_NAME}|:)(?<args>.*)"
       FUNCTION_RE = Matchers.regexp(FUNCTION_CALL)
 
       # COMPARATORS = {
