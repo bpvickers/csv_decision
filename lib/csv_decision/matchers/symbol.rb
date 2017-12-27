a9fc377c17baf07423f901d07456eeab9cc960a2
Brett Vickers <brett@phillips-vickers.com>
2017-12-27 12:50:01 -0500

    Update documentation.

--- lib/csv_decision/matchers/symbol.rb
+++ lib/csv_decision/matchers/symbol.rb
@@ -7,12 +7,10 @@
 module CSVDecision
   # Recognise expressions in table data cells.
   class Matchers
-    # Match cell against a
-    #   * cell constant - e.g., := true, = nil
-    #   * symbolic expression - e.g., :column, > :column
+    # Match cell against a symbolic expression - e.g., :column, > :column
     class Symbol < Matcher
-      # @param (see Matchers::Matcher#matches?)
-      # @return (see Matchers::Matcher#matches?)
+      # @param (see Matcher#matches?)
+      # @return (see Matcher#matches?)
       def matches?(cell)
         CSVDecision::Symbol.matches?(cell)
       end
